e84c88a64d7b75593a30b50691a78b2cd1560aa3
Ronan Lamour <rlamour@hellowork.com>
2026-02-19 11:34:53 +0100

    Fix formula: remove base64 error and complete file

--- Formula/pw.rb
+++ Formula/pw.rb
@@ -1,4 +1,3 @@
-base64: stdin: (null): error decoding base64 input stream
 class Pw < Formula
   desc "A fast, secure CLI password generator"
   homepage "https://github.com/r9r-dev/pw"
@@ -18,4 +17,18 @@ class Pw < Formula
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/r9r-dev/pw/releases/download/v0.1.1/pw-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "b966016e0d204fb2623dee2b11c6e42a0502e3b7910cac15
+      sha256 "b966016e0d204fb2623dee2b11c6e42a0502e3b7910cac15175075d918c2624f"
+    else
+      url "https://github.com/r9r-dev/pw/releases/download/v0.1.1/pw-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "6024e519e24f8e6e394359f05f00b4bc25e1d5b67e17b42fdfb1f68a08e389e2"
+    end
+  end
+
+  def install
+    bin.install "pw"
+  end
+
+  test do
+    assert_match(/\S{16}/, shell_output("#{bin}/pw -q"))
+  end
+end
