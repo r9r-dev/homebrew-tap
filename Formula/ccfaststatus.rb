721fdd61cc6c0b96a6826725a4287c97cacff668
Ronan Lamour <rlamour@hellowork.com>
2026-04-18 15:01:11 +0200

    ccfaststatus: v0.2.0

--- Formula/ccfaststatus.rb
+++ Formula/ccfaststatus.rb
@@ -1,7 +1,7 @@
 class Ccfaststatus < Formula
   desc "The Fastest Status Line for Claude Code, développée en Rust"
   homepage "https://github.com/r9r-dev/ccfaststatus"
-  version "0.1.0"
+  version "0.2.0"
   license "WTFPL"
 
   depends_on :macos
@@ -9,8 +9,8 @@ class Ccfaststatus < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/r9r-dev/ccfaststatus/releases/download/v0.1.0/ccfaststatus-0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "fc7ec9beb61c8f812894141b6a4abd9b4b4c773d1669921b8a83607256d11300"
+      url "https://github.com/r9r-dev/ccfaststatus/releases/download/v0.2.0/ccfaststatus-0.2.0-aarch64-apple-darwin.tar.gz"
+      sha256 "ac96123e2238786d8f813f2bb41053e8947727b21983588660936afdef33f307"
     end
   end
 
